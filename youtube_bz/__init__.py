af36d7839296df714d0fa3caedcf9dc396037ce4
Flowrey <flowrey@laposte.net>
2022-04-16 21:48:32 +0200

    Add docstring

--- youtube_bz/__init__.py
+++ youtube_bz/__init__.py
@@ -1,3 +1,9 @@
+"""YoutubeBrainz init module
+
+YoutubeBrainz allow you to find and download Youtube videos
+associated to an Album on MusicBrainz.
+"""
+
 import argparse
 import aiohttp
 import asyncio
@@ -35,6 +41,16 @@ ydl_opts = {
 
 
 def download(title, video_id):
+    """Download audio of a YouTube video.
+
+    Parameters
+    ----------
+    title : str
+        The track title.
+    video_id : str
+        The YouTube video id corresponding to the track.
+
+    """
     print('[Downloading] {} : {}'.format(title, video_id))
     ydl_opts['outtmpl'] = os.path.join('.', '{}.%(ext)s'.format(title))
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -43,6 +59,21 @@ def download(title, video_id):
 
 
 async def get_best_match(yt_initial_data, track):
+    """Get YouTube video corresponding to MusicBrainz track.
+
+    Parameters
+    ----------
+    yt_initial_data : dict
+        YouTube initial data.
+    track : dict
+        MusicBrainz track object.
+
+    Returns
+    -------
+    dict
+        A dict containing the video title and id, matching the track title.
+
+    """
     best_distance = None
     best_match = None
     for itemSectionRenderer in yt_initial_data['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']:
@@ -59,17 +90,58 @@ async def get_best_match(yt_initial_data, track):
 
 
 async def get_yt_intital_data(search_results):
+    """Get YouTube initial data.
+
+    Parameters
+    ----------
+    search_results : str
+        Raw search results containing YouTube initial data.
+
+    Returns
+    -------
+    dict
+        A dict containing the YouTube initial data.
+
+    """
     regex = r'(var\ ytInitialData\ =\ )(.*);</script><script'
     yt_initial_data = re.search(regex, search_results).group(2)
     return ujson.loads(yt_initial_data)
 
 
 async def get_search_query(release, track):
+    """Generate a search query for YouTube.
+
+    Parameters
+    ----------
+    release : dict
+        MusicBrainz release object.
+    track : dict
+        MusicBrainz track object.
+
+    Returns
+    -------
+    str
+        A search query for YouTube.
+
+    """
     search_query = f'"{release["artist-credit"][0]["name"]}" "{release["title"]}" "{track["title"]}" "Auto-generated"'
     return search_query
 
 
 async def get_yt_search_results(search_query):
+    """Get YouTube search results.
+
+    Parameters
+    ----------
+    search_query : str
+        The query to request to YouTube.
+
+    Returns
+    -------
+    str
+        Raw YouTube search results.
+
+    """
     async with aiohttp.ClientSession("https://www.youtube.com") as session:
         async with session.get('/results', params={'search_query': search_query}) as response:
             search_results = await response.text()
@@ -77,6 +149,19 @@ async def get_yt_search_results(search_query):
 
 
 async def get_musicbrainz_release(mbid):
+    """Get MusicBrainz release.
+
+    Parameters
+    ----------
+    mbid: str
+        MusicBrainz Identifier of a release.
+
+    Returns
+    -------
+    str
+        MusicBrainz release object.
+
+    """
     async with aiohttp.ClientSession('https://musicbrainz.org') as session:
         async with session.get(f'/ws/2/release/{mbid}', params={'inc': 'artists+recordings', 'fmt': 'json'}) as response:
             html = await response.text()
