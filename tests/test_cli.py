80b990b325be8b277176756ca3a82ac98c70d251
Flowrey <flowrey@laposte.net>
2022-05-21 13:58:17 +0200

    Add mocking for youtube_dl

--- tests/test_cli.py
+++
@@ -1,5 +0,0 @@
-import youtube_bz
-
-
-def test_main():
-    youtube_bz.main(['6c1adf00-edaf-4fe0-9d57-8dc5da90a4a9'])
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
 import asyncio
-import unittest
 import youtube_bz
 
+from unittest import IsolatedAsyncioTestCase, mock
+from pathlib import Path
 
 mbid_list = [
     'a17a48b6-51db-3c52-8fdd-066fb9989f78',
@@ -9,7 +10,25 @@ mbid_list = [
 ]
 
 
-class TestYoutubeBrainz(unittest.IsolatedAsyncioTestCase):
+def mocked_youtube_dl(*args):
+    class MockedYoutubeDL:
+        def __init__(self, ydl_opts):
+            self.ydl_opts = ydl_opts
+
+        def __enter__(self):
+            return self
+
+        def __exit__(sel, *args):
+            return
+
+        def download(self, ydl_id_list):
+            for i in ydl_id_list:
+                Path(self.ydl_opts['outtmpl'] % {'ext': 'mp3'}).touch()
+
+    return MockedYoutubeDL(*args)
+
+
+class TestYoutubeBrainz(IsolatedAsyncioTestCase):
 
     async def test_match(self):
         for mbid in mbid_list:
@@ -23,3 +42,12 @@ class TestYoutubeBrainz(unittest.IsolatedAsyncioTestCase):
                 self.assertIn('id', result)
                 self.assertIs(str, type(result['id']))
                 self.assertRegex(result['id'], r'^[A-z0-9_-]{11}$')
+
+    @mock.patch('youtube_dl.YoutubeDL', side_effect=mocked_youtube_dl)
+    def test_download(self, mock_youtube_dl):
+        youtube_bz.download('The Walls Are Way Too Thin', '36hmuTxo88U')
+        self.assertTrue(Path('The Walls Are Way Too Thin.mp3').is_file())
+
+    @mock.patch('youtube_dl.YoutubeDL', side_effect=mocked_youtube_dl)
+    def test_main(self, mocked_youtube_dl):
+        youtube_bz.main(['6c1adf00-edaf-4fe0-9d57-8dc5da90a4a9'])
