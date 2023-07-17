4b3acdd3e926d400987f701b9cb6640d7fda1966
Flowrey <flowrey@laposte.net>
2023-07-17 20:55:07 +0000

    add coverage for muscibrainz

--- tests/test_levenshtein.py
+++ tests/test_levenshtein.py
@@ -5,3 +5,4 @@ def test_levenshtein_distance():
     assert levenshtein_distance("test", "test") == 0
     assert levenshtein_distance("cats", "dogs") == 3
     assert levenshtein_distance("cats", "") == 4
+    assert levenshtein_distance("", "cats") == 4
---
+++ tests/test_youtube.py
@@ -0,0 +1,5 @@
+from unittest import IsolatedAsyncioTestCase
+
+class TestYoutube(IsolatedAsyncioTestCase):
+    async def test_raise_failed_to_parse_yt_initial_data(self):
+        pass
