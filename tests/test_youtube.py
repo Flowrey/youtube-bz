4a1369980bcea792d7b6174a06b2b0b8b2208c3f
Flowrey <flowrey@laposte.net>
2023-07-18 17:09:35 +0000

    add youtube test

--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -1,5 +1,9 @@
 from unittest import IsolatedAsyncioTestCase
+from youtube_bz.youtube import get_intital_data
+from youtube_bz.exceptions import FailedToParseYtIntialData
 
 class TestYoutube(IsolatedAsyncioTestCase):
     async def test_raise_failed_to_parse_yt_initial_data(self):
-        pass
+        data = "non_initial_data_regex"
+        with self.assertRaises(FailedToParseYtIntialData):
+            get_intital_data(data)
