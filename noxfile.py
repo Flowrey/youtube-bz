bd98f29d433662f2454758cbfcb32fe6c593c058
Flowrey <flowrey@laposte.net>
2023-10-14 12:44:10 +0200

    fix: fix poetry install

---
+++ noxfile.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import nox
+
+
+@nox.session
+def tests(session):
+    session.run("python", "-m", "pip", "install", "--upgrade", "pip")
+    session.install("-e", ".", "pytest", "pytest-cov", "pytest-httpserver")
+    session.run("pytest", "--cov=youtube_bz", "--cov-report=", "tests")
+    session.notify("cover")
+
+
+@nox.session
+def cover(session):
+    session.run("python", "-m", "pip", "install", "--upgrade", "pip")
+    session.install("coverage")
+    session.run("coverage", "report", "--show-missing", "--fail-under=95")
+    session.run("coverage", "lcov")
+    session.run("coverage", "erase")
+
+
+@nox.session
+def lint(session):
+    session.run("python", "-m", "pip", "install", "--upgrade", "pip")
+    session.install("black", "isort", "pyright", "mypy")
+    files = ["youtube_bz", "tests"] + [str(p) for p in Path(".").glob("*.py")]
+    session.run("black", "--check", *files)
+    session.run("isort", "--check", "--profile=black", *files)
+    session.run("mypy", "--install-types", "--check-untyped-defs", *files)
--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -1,26 +1,21 @@
-from unittest import IsolatedAsyncioTestCase
-from unittest.mock import patch
+import pytest
+from pytest_httpserver import HTTPServer
 
-from aiohttp import ClientSession
-
-from tests.utils import MockResponse
 from youtube_bz.api.youtube import Client, get_initial_data
 from youtube_bz.api.youtube.exceptions import FailedToParseIntialData
 
 
-class TestYoutube(IsolatedAsyncioTestCase):
-    async def test_raise_failed_to_parse_yt_initial_data(self):
-        data = "non_initial_data_regex"
-        with self.assertRaises(FailedToParseIntialData):
-            get_initial_data(data)
+def test_raise_failed_to_parse_yt_initial_data():
+    data = "non_initial_data_regex"
+    with pytest.raises(FailedToParseIntialData):
+        get_initial_data(data)
 
-    @patch("aiohttp.ClientSession.get")
-    async def test_get_initial_data(self, mock: ClientSession):
-        mock.return_value = MockResponse(
-            'var ytInitialData = {"foo":"bar"};</script><script', 200
-        )
-        client = await Client.new()
-        res = await client.get_search_results("Bring Me The Horizon")
-        await client.close()
-        init_data = get_initial_data(res)
-        assert init_data == {"foo": "bar"}
+
+def test_get_initial_data(httpserver: HTTPServer):
+    data = 'var ytInitialData = {"foo":"bar"};</script><script'
+    httpserver.expect_request("/results").respond_with_data(data)  # type: ignore
+    client = Client(httpserver.url_for("/results"))
+    res = client.get_search_results("Bring Me The Horizon")
+    init_data = get_initial_data(res)
+    print(init_data)
+    assert init_data == {"foo": "bar"}
